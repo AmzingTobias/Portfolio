9e7038020eda9d1a4e4fbd011882946bc4c5981c
Tobias <tobiasdunn.02@gmail.com>
2023-10-03 16:44:29 +0100

    Use Link attribute for redirection of product cards, renables server side rendering

--- app/projects/ProjectCard.tsx
+++ app/projects/ProjectCard.tsx
@@ -1,7 +1,5 @@
-"use client";
-
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 interface IProjectCardProps {
   title: string;
@@ -14,33 +12,29 @@ const ProjectCard: React.FC<IProjectCardProps> = ({
   img_source,
   mini_desc,
 }) => {
-  const router = useRouter();
-  const redirectToProject = () => {
-    router.push("/projects/" + title.replace(" ", "-"));
-  };
-
   return (
     <div
       style={{ maxWidth: 432 }}
       className="hover:cursor-pointer transition-colors duration-150 hover:bg-red-500 hover:bg-opacity-90"
-      onClick={redirectToProject}
     >
-      <Image
-        src={img_source}
-        alt={title}
-        width={432}
-        height={480}
-        className="bg-zinc-800 bg-opacity-30"
-      />
-      <div className="p-1">
-        <h3 className="mt-2 text-2xl font-semibold">{title}</h3>
-        {mini_desc && (
-          <>
-            <hr className="opacity-90 my-1.5" />
-            <p>{mini_desc}</p>
-          </>
-        )}
-      </div>
+      <Link href={`/projects/${title.replace(" ", "-")}`}>
+        <Image
+          src={img_source}
+          alt={title}
+          width={432}
+          height={480}
+          className="bg-zinc-800 bg-opacity-30"
+        />
+        <div className="p-1">
+          <h3 className="mt-2 text-2xl font-semibold">{title}</h3>
+          {mini_desc && (
+            <>
+              <hr className="opacity-90 my-1.5" />
+              <p>{mini_desc}</p>
+            </>
+          )}
+        </div>
+      </Link>
     </div>
   );
 };
