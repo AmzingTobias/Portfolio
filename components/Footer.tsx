f5f6543e3dc7c9177a7758d940cae4008ef01072
Tobias <tobiasdunn.02@gmail.com>
2023-10-04 17:47:45 +0100

    Add GitHub link to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 import { MdEmail } from "react-icons/md";
+import { AiFillGithub } from "react-icons/ai";
 
 const CONTACT_EMAIL = "tobias@tdmd.co.uk";
 
@@ -6,13 +7,24 @@ const Footer = () => {
   return (
     <footer className="items-center flex mx-auto justify-center mt-4">
       <div className="mx-0.5"></div>
-      <a
-        className="align-middle hover:font-semibold contents"
-        href={`mailto:${CONTACT_EMAIL}`}
-      >
-        <MdEmail />
-        &nbsp;{CONTACT_EMAIL}
-      </a>
+      <div className="flex items-center flex-col-reverse sm:flex-row">
+        <a
+          className="align-middle hover:font-semibold w-full mt-3 text-lg flex justify-center items-center"
+          href={`mailto:${CONTACT_EMAIL}`}
+        >
+          <MdEmail />
+          &nbsp;{CONTACT_EMAIL}
+        </a>
+        <div className="mx-3"></div>
+        <a
+          target="_blank"
+          className="align-middle hover:font-semibold w-full mt-3 text-lg flex justify-center items-center"
+          href={`https://github.com/AmzingTobias`}
+        >
+          <AiFillGithub />
+          &nbsp;GitHub
+        </a>
+      </div>
     </footer>
   );
 };
